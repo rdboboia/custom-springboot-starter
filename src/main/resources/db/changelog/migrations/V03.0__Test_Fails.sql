fa18969854b4339e8930fcb387eadb251f717d77
rdboboia <radulik95@gmail.com>
2024-03-20 15:32:39 +0100

    Added new script to test fails.

---
+++ src/main/resources/db/changelog/migrations/V03.0__Test_Fails.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+-- changeset liquibase:create_ok_table
+CREATE TABLE OkTable (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+-- rollback DROP TABLE OkTable;
+
+
+
+-- changeset liquibase:create_invalid_table
+CREATE TABLE InvalidTable (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+-- rollback DROP TABLE InvalidTable;
