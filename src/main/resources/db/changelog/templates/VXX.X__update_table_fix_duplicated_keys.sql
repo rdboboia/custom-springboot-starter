2e6c3960a39faa73a346ef7fb65789a592bea1ed
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-04-05 12:01:37 +0200

    Added template to fix duplicated entries.

--- src/main/resources/db/changelog/templates/VXX.X__update_table_fix_duplicated_keys.sql
+++ src/main/resources/db/changelog/templates/VXX.X__update_table_fix_duplicated_keys.sql
@@ -10,6 +10,7 @@
 --      - using the ID sequence + some hardcoded values.
 --      - using the UUID generation.
 -- Pick the one that best suits your needs.
+--
 -- As for the rollback, while technically possible with an auxiliary table, I don't find it very useful, so I left it empty.
 
 
@@ -17,7 +18,34 @@
 --  UPDATE TABLE <TABLE_NAME> randomize duplicated values
 --------------------------------------------------------
 
-
+--changeset liquibase:vXX.X_update_<column_name>_column_set_duplicates_to_random_uuid_values
+UPDATE
+    "<TABLE_NAME>"
+SET
+    "<COLUMN_NAME>" = LOWER(
+        SUBSTR(SYS_GUID(), 1, 8) || '-' ||
+        SUBSTR(SYS_GUID(), 9, 4) || '-' ||
+        '4' || SUBSTR(SYS_GUID(), 13, 3) || '-' ||
+        SUBSTR('89ab', DBMS_RANDOM.VALUE(1, 4), 1) || SUBSTR(SYS_GUID(), 17, 3) || '-' ||
+        SUBSTR(SYS_GUID(), 21, 12)
+    )
+WHERE
+    "<COLUMN_NAME>" IN (
+        -- Retrieve list of duplicateas sub-query
+        SELECT
+            "<COLUMN_NAME>"
+        FROM (
+            SELECT
+                COUNT(*) AS "COUNT_VAR", "<COLUMN_NAME>"
+            FROM
+                "<TABLE_NAME>"
+            GROUP BY
+                "<COLUMN_NAME>"
+        )
+        WHERE
+            "COUNT_VAR" > 1
+    );
+--rollback empty
 
 
 --------------------------------------------------------
