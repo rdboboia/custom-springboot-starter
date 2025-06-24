cbd0384d2e31d552b859caae310ceb9d694e92a8
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-06-24 18:57:01 +0200

    Changed script to match new entity.

--- src/main/resources/db/changelog/migrations/V04.0__create_model_product_tag.sql
+++ src/main/resources/db/changelog/migrations/V04.0__create_model_product_tag.sql
@@ -1,61 +1,24 @@
 --liquibase formatted sql
 
 --------------------------------------------------------
---  DDL for Table PRODUCT_TYPE
+--  DDL for Table PRODUCT_TAG
 --------------------------------------------------------
 
---changeset liquibase:v01.0_create_seq_product_type
-CREATE SEQUENCE "SEQ_PRODUCT_TYPE" START WITH 1 INCREMENT BY 1;
---rollback DROP "SEQUENCE "SEQ_PRODUCT_TYPE";
+--changeset liquibase:v04.0_create_seq_product_tag
+CREATE SEQUENCE "SEQ_PRODUCT_TAG" START WITH 1 INCREMENT BY 1;
+--rollback DROP "SEQUENCE "SEQ_PRODUCT_TAG";
 
 
---changeset liquibase:v01.0_create_table_product_type
-CREATE TABLE "PRODUCT_TYPE" (
-	"ID"			INTEGER			DEFAULT "SEQ_PRODUCT_TYPE".NEXTVAL		NOT NULL,
+--changeset liquibase:v04.0_create_table_product_tag
+CREATE TABLE "PRODUCT_TAG" (
+	"ID"			INTEGER			DEFAULT "SEQ_PRODUCT_TAG".NEXTVAL		NOT NULL,
 	"NAME"			VARCHAR2												NOT NULL
 );
---rollback DROP TABLE "PRODUCT_TYPE";
+--rollback DROP TABLE "PRODUCT_TAG";
 
 
---changeset liquibase:v01.0_create_pk_product_type
-ALTER TABLE "PRODUCT_TYPE"
-ADD CONSTRAINT "PK_PRODUCT_TYPE"
+--changeset liquibase:v04.0_create_pk_product_tag
+ALTER TABLE "PRODUCT_TAG"
+ADD CONSTRAINT "PK_PRODUCT_TAG"
 PRIMARY KEY ("ID");
---rollback ALTER TABLE "PRODUCT_TYPE" DROP CONSTRAINT "PK_PRODUCT_TYPE";
-
-
-
-
-
-
---------------------------------------------------------
---  DDL for Table PRODUCT
---------------------------------------------------------
-
---changeset liquibase:v01.0_create_seq_product
-CREATE SEQUENCE "SEQ_PRODUCT" START WITH 1 INCREMENT BY 1;
---rollback DROP SEQUENCE "SEQ_PRODUCT";
-
-
---changeset liquibase:v01.0_create_table_product
-CREATE TABLE "PRODUCT" (
-	"ID"					INTEGER		DEFAULT "SEQ_PRODUCT".NEXTVAL		NOT NULL,
-	"NAME"					VARCHAR2										NOT NULL,
-	"FK_PRODUCT_TYPE"		INTEGER											NOT NULL
-);
---rollback DROP TABLE "PRODUCT";
-
-
---changeset liquibase:v01.0_create_pk_product
-ALTER TABLE "PRODUCT"
-ADD CONSTRAINT "PK_PRODUCT"
-PRIMARY KEY ("ID");
---rollback ALTER TABLE "PRODUCT" DROP CONSTRAINT "PK_PRODUCT";
-
-
---changeset liquibase:v01.0_create_fk_product_type
-ALTER TABLE "PRODUCT"
-ADD CONSTRAINT "FK_PRODUCT_PRODUCT_TYPE"
-FOREIGN KEY ("FK_PRODUCT_TYPE")
-REFERENCES "PRODUCT_TYPE" ("ID");
---rollback ALTER TABLE "PRODUCT" DROP CONSTRAINT "FK_PRODUCT_PRODUCT_TYPE";
+--rollback ALTER TABLE "PRODUCT_TAG" DROP CONSTRAINT "PK_PRODUCT_TAG";
