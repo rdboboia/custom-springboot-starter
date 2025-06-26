9588092557a146e20dd27418a8ded7c43f094cc1
rdboboia <37208426+rdboboia@users.noreply.github.com>
2025-06-26 21:21:50 +0200

    Added wiremock dependency and configuration to start it on startup.

--- src/main/java/es/rdboboia/custom/starter/BackStarterApplication.java
+++ src/main/java/es/rdboboia/custom/starter/BackStarterApplication.java
@@ -1,16 +1,32 @@
 package es.rdboboia.custom.starter;
 
+import com.github.tomakehurst.wiremock.core.Options;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.cloud.contract.wiremock.WireMockSpring;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 /** Application entry point. */
+@Slf4j
+@AutoConfigureWireMock
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class BackStarterApplication {
 
   public static void main(String[] args) {
+    log.info("Starting application...");
     SpringApplication.run(BackStarterApplication.class, args);
+  }
+
+  @Bean
+  Options wireMockOptions() {
+    WireMockConfiguration options = WireMockSpring.options();
+    options.port(8090);
+    return options;
   }
 }
