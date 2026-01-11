e4ef8412f338ba53b51f07ddacf5f5a70c35bba0
rdboboia <37208426+rdboboia@users.noreply.github.com>
2026-01-11 14:08:18 +0100

    Wiremock server is now enabled only for default sprintboot profile.

--- src/main/java/es/rdboboia/custom/starter/configuration/WireMockServerBeanConfig.java
+++ src/main/java/es/rdboboia/custom/starter/configuration/WireMockServerBeanConfig.java
@@ -5,6 +5,7 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Configuration class to set up a WireMock server for mocking HTTP services. The server is
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @RequiredArgsConstructor
 @Configuration
+@Profile(value = {"default"})
 public class WireMockServerBeanConfig {
 
   private final WiremockConfigurationProperties properties;
