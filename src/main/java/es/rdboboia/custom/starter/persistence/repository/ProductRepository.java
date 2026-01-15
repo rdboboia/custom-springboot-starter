ae2b4779f3024985a946af68bf23d3589aac67a5
rdboboia <37208426+rdboboia@users.noreply.github.com>
2026-01-15 14:51:34 +0100

    Using entity graph.

--- src/main/java/es/rdboboia/custom/starter/persistence/repository/ProductRepository.java
+++ src/main/java/es/rdboboia/custom/starter/persistence/repository/ProductRepository.java
@@ -1,9 +1,18 @@
 package es.rdboboia.custom.starter.persistence.repository;
 
 import es.rdboboia.custom.starter.persistence.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /** {@link Product} repository. */
 public interface ProductRepository
-    extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {}
+    extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+  @Override
+  @EntityGraph(attributePaths = {Product.Fields.type, Product.Fields.tags})
+  Page<Product> findAll(Specification<Product> spec, Pageable pageable);
+}
